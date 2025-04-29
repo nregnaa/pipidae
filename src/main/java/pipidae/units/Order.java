5b3bfb1b5e00aabbfc8c3a2b9592356f500fbeaa
nregnaa <45128415+nregnaa@users.noreply.github.com>
2025-04-30 00:21:53 +0200

    Update Order.java

--- src/main/java/pipidae/units/Order.java
+++ src/main/java/pipidae/units/Order.java
@@ -3,18 +3,18 @@ public class Order{
     public Order(TitleRank orderRank, Action action){
         this.orderRank=orderRank;
         this.action=action;
-    };
+    }
     
     public TitleRank getOrderRank(){
         return orderRank;
-    };
+    }
     
     public Action getAction(){
         return action;
-    };
+    }
     
     //rank of the order dictates how important it is. The variable derives from the rank of its issuer.
     //Need.urgency equivalent.
     private TitleRank orderRank;
     private Action action;
-};
+}
