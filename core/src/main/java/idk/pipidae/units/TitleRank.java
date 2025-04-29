66110322c3383850472864ba487877080e08ada3
KasSsa_ <67292087+KasSsa-MC@users.noreply.github.com>
2025-04-30 01:39:26 +0200

    fixed TitleRank.java

--- core/src/main/java/idk/pipidae/units/TitleRank.java
+++ core/src/main/java/idk/pipidae/units/TitleRank.java
@@ -1,15 +1,10 @@
 package idk.pipidae.units;
 
 public enum TitleRank{
-    PEASENT(0), KNIGHT(1), BARON(2), COUNT(3), DUKE(4), KING(5);
-    
-    public TitleRank(int rank){
-        this.rank=rank;
-    }
-    
-    public void getRank(){
-        return rank;
-    }
-    
-    private final int rank;
+    PEASANT,
+    KNIGHT,
+    BARON,
+    COUNT,
+    DUKE,
+    KING
 }
