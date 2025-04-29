14a08425d1ef61764aef72d6424d88a5c579d264
nregnaa <45128415+nregnaa@users.noreply.github.com>
2025-04-30 00:20:15 +0200

    Create Need.java

---
+++ src/main/java/pipidae/units/Need.java
@@ -0,0 +1,20 @@
+
+public class Need{
+    public Need(int urgency){
+        this.urgency=urgency;
+        this.action=action;
+    }
+    
+    public int getUrgency(){
+        return urgency;
+    }
+
+    public Action getAction(){
+        return action;
+    }
+
+    //Order.orderRank equivalent, which will be used in part to evaluate which action is best.
+    //Denotes how important this action is.
+    private int urgency;
+    private Action action;
+}
