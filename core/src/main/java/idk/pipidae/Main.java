141dfbd0627d181b24eaf23cb789a0f366131e73
KasSsa_ <67292087+KasSsa-MC@users.noreply.github.com>
2025-04-30 01:48:30 +0200

    tried fixing line endings to be only LF

--- core/src/main/java/idk/pipidae/Main.java
+++ core/src/main/java/idk/pipidae/Main.java
@@ -16,6 +16,8 @@ public class Main extends ApplicationAdapter {
     public void create() {
         batch = new SpriteBatch();
         image = new Texture("libgdx.png");
+
+        //
     }
 
     @Override
