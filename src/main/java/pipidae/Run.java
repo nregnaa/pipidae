ab2c860cc77d9b948469ab9363d48d6b59ef5fb1
KasSsa_ <kassander.auve@gmail.com>
2025-04-29 23:31:31 +0200

    package structure

--- src/main/java/pipidae/Run.java
+++ src/main/java/pipidae/Run.java
@@ -1,6 +1,7 @@
+package pipidae;
 
 public class Run{
     public static void main(String[] args){
-        
-    };
-};
+        //
+    }
+}
--- src/main/java/pipidae/units/Title.java
+++ src/main/java/pipidae/units/Title.java
@@ -1,3 +1,4 @@
+package pipidae.units;
 
 public abstract class Title{
     public Title(String name, String title, int age, int health){
@@ -8,27 +9,31 @@ public abstract class Title{
     }
     public void update(List<> ){
         
-    };
-    protected void unitDecision();
-    protected void performDecision();
+    }
+    protected void unitDecision(){
+        //
+    }
+    protected void performDecision(){
+        //
+    }
     public String getName(){
         return name;
-    };
+    }
     public String getTitle(){
         return title;
-    };
+    }
     public int getAge(){
         return age;
-    };
+    }
     public int getHealth(){
         return health;
-    };
+    }
     public void setAge(int age){
         this.age=age;
-    };
+    }
     public void setHealth(int health){
         this.health=health;
-    };
+    }
     private String name;
     private String title;
     private int age;
